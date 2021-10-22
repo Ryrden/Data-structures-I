2580d8675fc30fe2837c16381f3e0c0b32180510
Ryan Souza <ryansateles@gmail.com>
2021-10-22 01:42:00 -0300

    função mover direita ok!

--- Trabalho1/game.c
+++ Trabalho1/game.c
@@ -1,5 +1,4 @@
 #include "game.h"
-#include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -42,6 +41,34 @@ char *search_year(GAME *game, int game_year) {
 
 void print_game_name(GAME *game) {
     printf("%s\n", game->name);
+}
+
+boolean swap_games(GAME *game1, GAME *game2) {
+
+    if (game1 != NULL && game2 != NULL) {
+        char temp_name[256];
+        char temp_producer[256];
+        int temp_year;
+        int temp_key;
+
+        strcpy(temp_name, game1->name);
+        strcpy(temp_producer, game1->producer);
+        temp_year = game1->year;
+        temp_key = game1->key;
+
+        strcpy(game1->name, game2->name);
+        strcpy(game1->producer, game2->producer);
+        game1->year = game2->year;
+        game1->key = game2->key;
+
+        strcpy(game2->name, temp_name);
+        strcpy(game2->producer, temp_producer);
+        game2->year = temp_year;
+        game2->key = temp_key;
+
+        return TRUE;
+    }
+    return FALSE;
 }
 
 int get_key(const GAME *game) {
--- Trabalho1/game.h
+++ Trabalho1/game.h
@@ -1,5 +1,6 @@
 #ifndef JOGO_H
 #define JOGO_H
+#include "util.h"
 
 typedef struct game_st GAME;
 
@@ -8,6 +9,8 @@ GAME *register_game(char *name, char *producer, int year, int key);
 int get_key(const GAME *game);
 
 GAME *create_empty_item();
+
+boolean swap_games(GAME *game1, GAME *game2);
 
 char *search_producer(GAME *game, char *game_producer);
 
