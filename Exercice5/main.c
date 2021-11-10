31992c1b8d7e9796f3c255c6c8e3b978a8bd1897
Ryan Souza <ryansateles@gmail.com>
2021-11-10 12:19:24 -0300

    Lógica inicial construiada, necessário verificar as funções e o uso do TAD

--- Exercice5/main.c
+++ Exercice5/main.c
@@ -1,5 +1,8 @@
+#include "Bank/Bank.h"
+#include "Tree/Tree.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*INFORMAÇÕES:
     NOME: Ryan Souza Sá Teles
@@ -12,28 +15,65 @@
 
 int main() {
 
-    //Criar Arvore
+    // Criar Arvore
+    TREE *tree;
+    tree = create_tree();
 
-    //Pegar entrada
-    //Pegar Linha
-    //Pegar CPF
-    //Pegar Nome
-    //Pegar Idade
-    //Pegar Saldo
+    // Pegar entrada
+    int n;
+    scanf("%d", &n);
 
-    //Comparar CPF
-    //Se maior -> Direita
-    //Inserir na Arvore
-    //Se menor -> Esquerda
-    //Inserir na Arvore
+    char line[256];
+    int CPF_root;
+    for (int i = 0; i < n; i++) {
+        // Pegar Linha
+        fgets(line, 256, stdin);
+        line[strlen(line) - 1] = '\0';
 
-    //Imprimir In-Order
+        // Pegar CPF
+        char *CPF_string = strtok(line, ";");
+        // tratar entrada - retirar "." e "-"
+        int CPF = get_cpf_numbers(CPF_string);
 
-    //Imprimir Pre-Order
-    
-    //Imprimir Post-Order
+        // define raiz
+        if (i == 0)
+            CPF_root = CPF;
 
-    //Deletar Arvore
-    
+        // Pegar Nome
+        char *name = strtok(NULL, ";");
+        name[strlen(line) - 1] = '\0';
+
+        // Pegar Idade
+        int age = atoi(strtok(NULL, ";"));
+
+        // Pegar Saldo
+        int balance = atoi(strtok(NULL, "\n"));
+
+        // Criar cadastro no banco
+        printf("%d %s %d %d", CPF, name, age, balance);
+        // Comparar CPF
+        if (compare_CPF(CPF, CPF_root)) {
+            // Se maior -> Direita
+            // Inserir na Arvore
+            /* insert_tree(tree, , RIGHT, CPF); */
+        } else {
+            // Se menor -> Esquerda
+            // Inserir na Arvore
+            /* insert_tree(tree, , LEFT, CPF); */
+        }
+    }
+
+    // Imprimir In-Order
+    in_order_tree(tree);
+
+    // Imprimir Pre-Order
+    pre_order_tree(tree);
+
+    // Imprimir Post-Order
+    pos_order_tree(tree);
+
+    // Deletar Arvore
+    erase_tree(&tree);
+
     return EXIT_SUCCESS;
 }
