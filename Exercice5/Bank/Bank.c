6358cbc6513fc3c03ed187511114eaefd3afe974
Ryan Souza <ryansateles@gmail.com>
2021-11-10 12:18:16 -0300

    declarei funções que serão futuramente criadas

--- Exercice5/Bank/Bank.c
+++ Exercice5/Bank/Bank.c
@@ -1,3 +1,9 @@
 #include <stdlib.h>
 #include "Bank.h"
 
+struct bank_st {
+    char name[256];
+    unsigned int CPF;
+    unsigned int age;
+    long long int balance;
+};
--- Exercice5/Bank/Bank.h
+++ Exercice5/Bank/Bank.h
@@ -1,6 +1,13 @@
 #ifndef BANK_H
 #define BANK_H
+#include "../Util/util.h"
 
+typedef struct bank_st BANK;
 
+BANK create_bank_item(char *name, int CPF, int age, int balance);
+int get_cpf_numbers(char *CPF);
+int get_key(BANK *bank);
+void print_item(BANK *bank);
+boolean compare_CPF(int CPF_1, int CPF_2);
 
 #endif //BANK_H
