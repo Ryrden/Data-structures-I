3d18c8bac8b7bd48e16e94c6328ad05c6edd976c
Ryan Souza <ryansateles@gmail.com>
2021-11-10 12:18:52 -0300

    necessário modificações, estrutura base quase OK

--- Exercice5/Tree/Tree.c
+++ Exercice5/Tree/Tree.c
@@ -1,2 +1,120 @@
-#include <stdlib.h>
 #include "Tree.h"
+#include <stdlib.h>
+#include "../Bank/Bank.h"
+
+typedef struct node_st NODE;
+
+struct node_st {
+    void *item;
+    NODE *right;
+    NODE *left;
+};
+
+struct tree_st {
+    NODE *root;
+    int depth;
+};
+
+static void recursion_pre_order(NODE *root);
+static void recursion_in_order(NODE *root);
+static void recursion_pos_order(NODE *root);
+static void erase_tree_nodes(NODE **root)
+
+TREE *create_tree() {
+    TREE *tree;
+    tree = (TREE *)malloc(sizeof(TREE));
+    if (tree != NULL) {
+        tree->root = NULL;
+        tree->depth = -1;
+        return tree;
+    }
+    return NULL;
+}
+
+NODE *create_tree_node(void *item) {
+    if (item != NULL) {
+        NODE *new_item;
+        new_item = (NODE *)malloc(sizeof(NODE));
+        if (new_item == NULL)
+            exit(EXIT_FAILURE);
+        new_item->item = item;
+        new_item->left = NULL;
+        new_item->right = NULL;
+        return new_item;
+    }
+    return NULL;
+}
+
+void pre_order_tree(TREE *tree) {
+    recursion_pre_order(tree->root);
+}
+
+void in_order_tree(TREE *tree) {
+    recursion_in_order(tree->root);
+}
+
+void pos_order_tree(TREE *tree) {
+    recursion_pos_order(tree->root);
+}
+
+boolean insert_tree(TREE *tree, void *item, int side, int key) {
+    if (tree->root == NULL)
+        return ((tree->root = create_tree_node(item)) != NULL);
+    else
+        return ((tree->root = insert_tree_node(tree, item, side, key)));
+}
+
+NODE insert_tree_node(NODE *root, void *item, int side, int key) {
+    if (root != NULL) {
+        root->left = insert_tree_node(root->left, item, side, key);
+        root->right = insert_tree_node(root->right, item, side, key);
+        if (key == get_key(item)) {
+            if (side == LEFT && root->left == NULL)
+                root->left = create_tree_node(item);
+            else if (side == RIGHT && root->right == NULL)
+                root->right = create_tree_node(item);
+        }
+    }
+    return root;
+}
+
+boolean erase_tree(TREE **tree) {
+    if (*tree != NULL) {
+        erase_tree_nodes(&tree->root);
+        return TRUE;
+    }
+    return FALSE
+}
+
+
+static void erase_tree_nodes(NODE **root) {
+    if (root != NULL) {
+        erase_tree_nodes(&root->left);
+        erase_tree_nodes(&root->right);
+        free(&root);
+    }
+}
+
+static void recursion_pre_order(NODE *root) {
+    if (root != NULL) {
+        print_item(root->item);
+        recursion_pre_order(root->left);
+        recursion_pre_order(root->right);
+    }
+}
+
+static void recursion_in_order(NODE *root) {
+    if (root != NULL) {
+        print_item(root->item);
+        recursion_in_order(root->left);
+        recursion_in_order(root->right);
+    }
+}
+
+static void recursion_pos_order(NODE *root) {
+    if (root != NULL) {
+        print_item(root->item);
+        recursion_pos_order(root->left);
+        recursion_pos_order(root->right);
+    }
+}
--- Exercice5/Tree/Tree.h
+++ Exercice5/Tree/Tree.h
@@ -1,6 +1,18 @@
 #ifndef TREE_H
 #define TREE_H
+#include "../Util/util.h"
 
+#define LEFT 0
+#define RIGHT 1
 
+typedef struct tree_st TREE;
 
-#endif //TREE_H
+TREE *create_tree();
+NODE *create_tree_node(void *item);
+boolean erase_tree(TREE **tree);
+void pre_order_tree(TREE *tree);
+void in_order_tree(TREE *tree);
+void pos_order_tree(TREE *tree);
+boolean insert_tree(TREE *tree, void *item, int side, int key);
+
+#endif // TREE_H
